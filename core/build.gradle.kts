01015c4fc44c8da96b563f892ecaee595b186c1e
Lukas Hanke <lukas.hanke11@gmail.com>
2020-12-31 13:20:26 +0100

    moved antlr generated sources to separate sourceSet apart from main

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -11,12 +11,26 @@ dependencies {
 }
 
 sourceSets {
-    main {
+    val antlrGen by creating {
         java {
-            srcDirs(
-                    "src/main/java",
-                    "src-gen/main/java"
-            )
+            setSrcDirs(listOf("src-gen/main/java"))
+        }
+        dependencies {
+            add("antlrGenImplementation", "org.antlr:antlr4:${antlrVersion}")
+        }
+    }
+    val main by getting {
+        java {
+            setSrcDirs(listOf("src/main/java")) // remove paths appended by antlr plugin
+            compileClasspath += antlrGen.output
+            runtimeClasspath += antlrGen.output
+        }
+    }
+    val test by getting {
+        java {
+            setSrcDirs(listOf("src/test/java")) // remove paths appended by antlr plugin
+            compileClasspath += antlrGen.output
+            runtimeClasspath += antlrGen.output
         }
     }
 }
@@ -24,15 +38,25 @@ sourceSets {
 tasks {
     generateGrammarSource {
         maxHeapSize = "64m"
-        outputDirectory = File("${projectDir}/src-gen/main/java")
+        outputDirectory = File(project.sourceSets.getByName("antlrGen").allJava.srcDirs.stream().findFirst().get().absolutePath)
     }
 
     clean {
         doLast {
-            project.delete(fileTree("src-gen/main/java"))
-            File("src-gen/main/java").listFiles()?.forEach {
-                project.delete(it)
+            project.sourceSets.getByName("antlrGen").allJava.srcDirs.forEach {
+                project.delete(fileTree(it.absolutePath))
+                File(it.absolutePath).listFiles()?.forEach {
+                    project.delete(it)
+                }
             }
         }
+    }
+
+    compileJava {
+        dependsOn("compileAntlrGenJava")
+    }
+
+    getByName("compileAntlrGenJava") {
+        dependsOn("generateGrammarSource")
     }
 }
